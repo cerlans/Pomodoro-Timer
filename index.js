dfd8c7ccfed85880cd16bc17a3b590fac52208ce
Kevin <35633054+cerlans@users.noreply.github.com>
2021-01-26 11:27:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const startButton = document.getElementsByClassName('start')[0];
 
 // the 'clock' number that ticks down
 let number = document.getElementById('text')
-// this number is an h1 with the default value off '25:00', it is the target for the stopwatch functionality
+// this number is an h1 with the default value off '25:00', it is the main target for the stopwatch functionality
 
+
+let textTime = '25:00'
+// this variable is a mutable string, the base string value will be mutated to another string value in accordance 
+// to the option the user has selected IE: if user clicks short break, it will be a string that reads '5:00'
+// it also acts as a reference when the timer is reset, setting the textContent off the number variable to the corresponding break length selected.
 let startTimer = (duration,display) => {
     let timer = duration, minutes, seconds;
     setInterval(() => {
@@ -25,6 +30,7 @@ let startTimer = (duration,display) => {
     
             if (--timer < 0) {
                 reset();
+                // will end the interval when it reaches 0
                 number.textContent = '0:00';
             }
         } else {
@@ -43,7 +49,8 @@ function reset(){
     for (let i = 0 ; i < highestTimeoutId ; i++) {
     clearTimeout(i); 
 }
-    
+number.innerText = textTime;
+startButton.disabled = 'false';   
 }
 
 breakButtons.addEventListener('click', (event)=> {
